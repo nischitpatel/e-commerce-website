22873b84a4ee913a5bbd44721590b0588cbb3d2c
nischitpatel <np58845@gmail.com>
2025-11-09 15:39:40 -0500

    Added a 'Users' functionality

--- ecommerce_backend/users/views.py
+++ ecommerce_backend/users/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from rest_framework import generics, status
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from django.contrib.auth.models import User
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.tokens import RefreshToken
+from .serializers import RegisterSerializer, UserSerializer
 
-# Create your views here.
+# Register new user
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
+
+
+# Custom login view using JWT
+class LoginView(TokenObtainPairView):
+    pass  # SimpleJWT already handles this
+
+
+# Get logged-in user profile
+class ProfileView(generics.RetrieveAPIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = UserSerializer
+
+    def get_object(self):
+        return self.request.user
