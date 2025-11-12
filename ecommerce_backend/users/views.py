845b4299624e29eed4734dd6db9137e3e0ac05a5
nischitpatel <np58845@gmail.com>
2025-11-12 09:04:11 -0500

    Modified the register endpoint to send token in the response

--- ecommerce_backend/users/views.py
+++ ecommerce_backend/users/views.py
@@ -12,6 +12,28 @@ class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = RegisterSerializer
 
+    def create(self, request, *args, **kwargs):
+        # Validate and save user
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+
+        # Generate JWT tokens
+        refresh = RefreshToken.for_user(user)
+
+        # Prepare custom response
+        response_data = {
+            "refresh": str(refresh),
+            "access": str(refresh.access_token),
+            "user": {
+                "id": user.id,
+                "username": user.username,
+                "email": user.email
+            }
+        }
+
+        return Response(response_data, status=status.HTTP_201_CREATED)
+
 
 class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
     def validate(self, attrs):
@@ -19,10 +41,13 @@ class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
         data = super().validate(attrs)
 
         # add extra field to the response
-        data["username"] = self.user.username
+        data["user"] = {
+            "id": self.user.id,
+            "username": self.user.username,
+            "email": self.user.email
+        }
 
         return data
-
 
 # Custom login view using JWT
 class LoginView(TokenObtainPairView):
