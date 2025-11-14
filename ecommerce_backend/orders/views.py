2320f365281abf435852f1e9f918704008c4be4a
nischitpatel <np58845@gmail.com>
2025-11-13 20:20:20 -0500

    Cart service

--- ecommerce_backend/orders/views.py
+++ ecommerce_backend/orders/views.py
@@ -1,22 +1,22 @@
-from rest_framework import viewsets, permissions
-from .models import Order
-from .serializers import OrderSerializer
+# from rest_framework import viewsets, permissions
+# from .models import Order
+# from .serializers import OrderSerializer
 
-class OrderViewSet(viewsets.ModelViewSet):
-    queryset = Order.objects.all()
-    serializer_class = OrderSerializer
-    permission_classes = [permissions.IsAuthenticated]
+# class OrderViewSet(viewsets.ModelViewSet):
+#     queryset = Order.objects.all()
+#     serializer_class = OrderSerializer
+#     permission_classes = [permissions.IsAuthenticated]
 
-    def perform_create(self, serializer):
-        # Auto-assign logged-in user when order is created
-        serializer.save(user=self.request.user)
+#     def perform_create(self, serializer):
+#         # Auto-assign logged-in user when order is created
+#         serializer.save(user=self.request.user)
 
-    def get_queryset(self):
-        # Users see only their own orders
-        user = self.request.user
-        if user.is_staff:
-            return Order.objects.all()
-        return Order.objects.filter(user=user)
+#     def get_queryset(self):
+#         # Users see only their own orders
+#         user = self.request.user
+#         if user.is_staff:
+#             return Order.objects.all()
+#         return Order.objects.filter(user=user)
     
 # orders/views.py
 
@@ -35,7 +35,7 @@ class OrderViewSet(viewsets.ModelViewSet):
         product = serializer.validated_data['product']
         quantity = serializer.validated_data['quantity']
 
-        # 🧮 Calculate total price securely
+        # Calculate total price 
         total_price = product.price * quantity
 
         # Save with correct total_price & logged-in user
@@ -45,7 +45,6 @@ class OrderViewSet(viewsets.ModelViewSet):
         product = serializer.validated_data.get('product', serializer.instance.product)
         quantity = serializer.validated_data.get('quantity', serializer.instance.quantity)
 
-        # 🧮 Recalculate on update
         total_price = product.price * quantity
 
         serializer.save(total_price=total_price)
--- ecommerce_backend/store/models.py
+++ ecommerce_backend/store/models.py
@@ -4,6 +4,7 @@ class Product(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.PositiveIntegerField(default=0)
     image = models.URLField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
